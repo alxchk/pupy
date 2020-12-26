572e51a3936985385e537cd3bef31e63578aab3f
Oleksii Shevchuk <alxchk@gmail.com>
2020-12-26 23:55:13 +0200

    msgbox/windows: use unicode strings

--- pupy/packages/windows/all/pupwinutils/msgbox.py
+++ pupy/packages/windows/all/pupwinutils/msgbox.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
-# -*- coding: UTF8 -*-
+# -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
 import ctypes
 import threading
 
+from network.lib.convcompat import as_unicode_string
+
+
 def MessageBox(text, title):
-    t=threading.Thread(target=ctypes.windll.user32.MessageBoxA, args=(None, text, title, 0))
+    text = as_unicode_string(text, fail=False)
+    title = as_unicode_string(title, fail=False)
+
+    t = threading.Thread(
+        target=ctypes.windll.user32.MessageBoxW, args=(
+            None, text, title, 0
+        )
+    )
+
     t.daemon=True
     t.start()
